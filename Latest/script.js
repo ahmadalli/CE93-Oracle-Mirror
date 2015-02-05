bc94f4742700b1f16ae1ed23ab34dd29825f2097
ahmadali shafiee <mail@ahmadalli.net>
2015-02-06 01:22:07 +0330

    Files created

---
+++ Latest/script.js
@@ -0,0 +1,4 @@
+function interceptRequest(request) {
+  return { redirectUrl: request.replace("docs.oracle.com","oracle.ce93.ir") }
+}
+chrome.experimental.webRequest.onBeforeRequest.addListener(interceptRequest, { urls: ['*://docs.oracle.com/*'] }, ['blocking']);
